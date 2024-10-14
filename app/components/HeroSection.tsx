be7fa4365aac7dfc995f91f906c38995efc8dfd0
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-10-14 11:49:12 +0530

    Refactor HeroSection and Navigation components

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -218,7 +218,7 @@ const HeroSection: React.FC = () => {
     }, []);
 
     return (
-        <div ref={headerRef} className="large-header relative flex flex-col items-center justify-center min-h-screen text-white text-center px-4 sm:px-8 bg-[#000040] overflow-hidden">
+        <div ref={headerRef} className="large-header relative flex flex-col items-center justify-center pb-12 min-h-screen text-white text-center px-4 sm:px-8 bg-[#000040] overflow-hidden">
             <canvas ref={canvasRef} id="demo-canvas"></canvas>
             <div className="relative z-10">
                 <h1 className="text-4xl sm:text-6xl lg:text-8xl font-bold mb-4">
